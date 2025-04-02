f263417af2236743dde989d494531a7f12a47605
Hudson Pryde <hudson.stuart@torontomu.ca>
2025-04-02 16:56:31 -0400

    2025-04-02

---
+++ 2025-04/lc2873.py
@@ -0,0 +1,18 @@
+from typing import List
+
+
+class Solution:
+    def maximumTripletValue(self, nums: List[int]) -> int:
+        n = len(nums)
+        max_val = 0
+        max_diff = float('-inf')
+        max_trip = 0
+        for i in range(n):
+            num = nums[i]
+            max_trip = max(max_trip,max_diff*num)
+            max_diff = max(max_diff,max_val-num)
+            max_val = max(max_val,num)
+        return max_trip
+
+s = Solution()
+print(s.maximumTripletValue([2,3,1]))
